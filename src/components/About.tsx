44bb6b2bec7a7a7b6d0f169fbb074d0b599f08dd
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2021-08-29 20:51:40 +0700

    success

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className='text-center space-y-3'>
             <p className='text-2xl font-semibold'>About me</p>
-            <p className='text-xl'>6306121006 Pongtanate Namsawat</p>cmd
+            <p className='text-xl'>630612106 Pongtanate Namsawat</p>
         </div>
     )
 }
