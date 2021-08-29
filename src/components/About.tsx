7326a6e665d95923e19557c8002a9787f8e1f902
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2021-08-29 20:28:31 +0700

    gg

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>6306121006 Pongtanate Namsawat</p>cmd
+        </div>
+    )
+}
+
+export default About
